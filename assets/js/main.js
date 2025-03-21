c84d8a6eb83083b9da24c413473aa7deca4435fb
Muhammad Farihin Mushawwir <mfarihinmushawwir@gmail.com>
2025-03-21 10:45:40 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,127 @@
+
+$(function () {
+
+    "use strict";
+
+    //===== Prealoder
+
+    $(window).on('load', function (event) {
+        $('.preloader').delay(500).fadeOut(500);
+    });
+
+
+  //===== Sticky
+
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".header_navbar").removeClass("sticky");
+        } else {
+            $(".header_navbar").addClass("sticky");
+        }
+    });
+    
+    
+    //===== Section Menu Active
+
+    var scrollLink = $('.page-scroll');
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 73;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        });
+    });
+    
+    
+    //===== close navbar-collapse when a  clicked
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+    $(".navbar-toggler").on('click', function () {
+        $(this).toggleClass("active");
+    });
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-toggler").removeClass('active');
+    });    
+
+
+    ///===== Progress Bar
+
+    if ($('.progress_line').length) {
+        $('.progress_line').appear(function () {
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width', percent + '%');
+        }, {
+            accY: 0
+        });
+    }
+    // tes
+    if ($(prompt.apply))
+        if($(this).scrollLeft()>600) {
+            $(this).add(AudioBuffer)
+        }else{
+            $(this).remove.apply
+        }
+        if($(prompt.apply))
+            if($(this).scrollLeft () >900){
+                $(this).add(AudioBuffer);
+                $
+            }
+        
+
+
+
+    //===== Back to top
+
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200)
+        } else {
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+
+
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+
+
+});
+
+//Slider
+let currentIndex = 0;
+
+function moveSlide(step) {
+  const slides = document.querySelector('.slides');
+  const totalSlides = document.querySelectorAll('.slide').length;
+  
+  currentIndex += step;
+
+  if (currentIndex < 0) {
+    currentIndex = totalSlides - 1;
+  } else if (currentIndex >= totalSlides) {
+    currentIndex = 0;
+  }
+
+  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
